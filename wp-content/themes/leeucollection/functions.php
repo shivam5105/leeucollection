49bea4c074d39f71cb018fa8a1b7b84d6aa270eb
Tanmay Vats <tanmay27vats@gmail.com>
2017-04-12 17:51:58 +0530

    svg support

--- wp-content/themes/leeucollection/functions.php
+++ wp-content/themes/leeucollection/functions.php
@@ -1181,11 +1181,12 @@ function cc_mime_types($mimes) {
 add_filter('upload_mimes', 'cc_mime_types');
 
 function fix_svg_thumb_display() {
-  echo '
+  echo '<style type="text/css">
     td.media-icon img[src$=".svg"], img[src$=".svg"].attachment-post-thumbnail { 
       width: 100% !important; 
       height: auto !important; 
     }
+   </style>
   ';
 }
 add_action('admin_head', 'fix_svg_thumb_display');
