2b75716234b9fc621389ae7da0af756a80ea2beb
Tanmay Vats <tanmay27vats@gmail.com>
2017-05-16 15:23:11 +0530

    changes

--- wp-content/themes/leeucollection/404.php
+++ wp-content/themes/leeucollection/404.php
@@ -8,27 +8,16 @@
  */
 
 get_header(); ?>
+    <section id="site-main">
+		<div class="container">
+			<div class="text-center scroll-anim animate-custom" data-anim="fade-up">
+				<h1 class="ucase"><?php _e( 'Oops! That page can&rsquo;t be found.', 'leeucollection' ); ?></h1>
+			</div>
+			<div class="page-content">
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'leeucollection' ); ?></p>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentysixteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentysixteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-
-		<?php get_sidebar( 'content-bottom' ); ?>
-
-	</div><!-- .content-area -->
-
-
+				<?php get_search_form(); ?>
+			</div><!-- .page-content -->
+		</div>
+	</section>
 <?php get_footer(); ?>
