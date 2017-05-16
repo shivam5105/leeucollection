aed5f11f43ee54bb38b25a4c22184e38d185ff7a
info@yogasmoga.com <info@yogasmoga.com>
2017-05-16 18:37:08 +0530

    artisan page

--- wp-content/themes/leeucollection/inc/post-meta.php
+++ wp-content/themes/leeucollection/inc/post-meta.php
@@ -647,8 +647,24 @@ Container::make('post_meta', 'Content Section')
             )),
         )),
     ));
+
+// Artisan page
+Container::make('post_meta', 'Artisan Drinks Content Section')
+    ->show_on_post_type($leeu_post_types)
+    ->show_on_template('template-artisan-drinks.php')
+    ->add_fields(array(
+        Field::make('complex', 'crb_artisan_drinks_content', '')->add_fields(array(
+            Field::make('image', 'crb_artisan_drink_image1', 'Full Image')->help_text('(Image Dimensions (WxH): 821 x 478)'),
+            Field::make('image', 'crb_artisan_drink_image2', 'Half Left Image')->help_text('(Image Dimensions (WxH): 411 x 258)')->set_width('50'),
+            Field::make('image', 'crb_artisan_drink_image3', 'Half Right Image')->help_text('(Image Dimensions (WxH): 411 x 258)')->set_width('50'),
+            Field::make('text', 'crb_artisan_drinks_section_heading', 'Heading'),
+            Field::make('textarea', 'crb_artisan_drinks_section_description', 'Content'),
+        )),
+    )); 
+
 /*
 //Slider Info
+
 
 
 //The Chef
