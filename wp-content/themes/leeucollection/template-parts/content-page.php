7af0018d57bc9d4cd0de168f0fa2efb582254008
info@yogasmoga.com <info@yogasmoga.com>
2017-06-08 18:14:10 +0530

    lelo

--- wp-content/themes/leeucollection/template-parts/content-page.php
+++ wp-content/themes/leeucollection/template-parts/content-page.php
@@ -10,12 +10,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="leeu-heading-wrap scroll-anim" data-anim="fade-up">
+			<div class="text-center">
+				<?php the_title( '<h1 class="entry-title ucase">', '</h1>' ); ?>
+			</div>	
+		</div>
 	</header><!-- .entry-header -->
 
 	<?php leeucollection_post_thumbnail(); ?>
 
-	<div class="entry-content">
+	<div class="entry-content scroll-anim" data-anim="fade-up" data-anim-delay="100">
 		<?php
 		the_content();
 
@@ -29,7 +33,26 @@
 		) );
 		?>
 	</div><!-- .entry-content -->
-
+	<div class="linking-wrap"> 
+		<div class="row">
+			<div class="col-6 rm-pad">
+				<div class="two-section-link"> 
+					<div class="section-content">Leeu Estates Executive Suite LC</div>
+					<div class="cstm-btn-wrapper contact-email">
+						<a href="javascript:void(0)" class="cstm-btn arrow-btn text-center">REQUEST</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 rm-pad">
+				<div class="two-section-link"> 
+					<div class="section-content">Leeu Estates Executive Suite LC</div>
+					<div class="cstm-btn-wrapper contact-email">
+						<a href="javascript:void(0)" class="cstm-btn arrow-btn text-center">REQUEST</a>
+					</div>
+				</div>					
+			</div>
+		</div>
+	</div>	
 	<?php
 		edit_post_link(
 			sprintf(
