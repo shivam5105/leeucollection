3bc84986933ec8e5800b1f7020afb8758f3da106
info@yogasmoga.com <info@yogasmoga.com>
2017-07-19 10:22:02 +0530

    Quire task

--- wp-content/themes/leeucollection/page.php
+++ wp-content/themes/leeucollection/page.php
@@ -12,23 +12,64 @@
  */
 
 get_header(); ?>
+<?php
+	$post_id 	= $post->ID;
+	$hotel_id 	= get_hotel_id($post_id);
+    $post_meta 	= ( $post ) ? get_post_meta( $post->ID ) : null;
+
+	$top_most_parent_post = ($hotel_id == false) ? false : get_post($hotel_id);
+
+	$page_heading = (@$post_meta['_crb_page_heading'][0]) ? $post_meta['_crb_page_heading'][0] : $post->post_title;
+    ?>
     <section id="site-main">
+    	<div class="container">
+			<div class="leeu-heading-wrap scroll-anim" data-anim="fade-up">
+				<div class="row">
+					<div class="col-2 rm-pad"></div>
+					<div class="col-8 rm-pad">
+						<div class="text-center">
+							<!-- <div class="leeu-text ucase" itemprop="legalName"><?php echo $top_most_parent_post->post_title;?></div> -->
+							<h1 class="ucase" itemprop="name"><?php echo $page_heading; ?></h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="container">
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+			 <div class="col-12 rm-pad room-listing-contain">
+				<div class="row listing-row common-pages">
+					<div class="col-2 rm-pad-left">
+						<div class="side-nav-contain">
+							<div class="scroll-anim" data-anim="fade-up">
+								<div class="side-nav-wrap common-pages-sidebar">
+									<?php
+									include_once("leeu_sidebar.php");
+									?>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-8">
+						<div class="scroll-anim" data-anim="fade-up">
+							<?php
+							// Start the loop.
+							while ( have_posts() ) : the_post();
 
-				// Include the page content template.
-				get_template_part( 'template-parts/content', 'page' );
+								// Include the page content template.
+								get_template_part( 'template-parts/content', 'page' );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) {
+									comments_template();
+								}
 
-				// End of the loop.
-			endwhile;
-			?>
+								// End of the loop.
+							endwhile;
+							?>
+						</div>
+					</div>
+				</div>
+			</div>				
 		</div>
 	</section>
 <?php get_footer(); ?>
