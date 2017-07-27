bef281d19e93f2a2aae6ff2429af43a9cd240e3a
Tanmay Vats <tanmay27vats@gmail.com>
2017-07-27 18:01:02 +0530

    changes

--- wp-content/themes/leeucollection/template-room.php
+++ wp-content/themes/leeucollection/template-room.php
@@ -122,7 +122,13 @@ get_header(); ?>
 												<?php //echo @$post_meta['_crb_rate_amount'][0]; ?>
 												<?php //echo @$post_meta['_crb_rate_for'][0]; ?>
 											</div> -->
-											<a class="cstm-btn arrow-btn text-center popup-booking-button-anchor" href="javascript:void(0);" data-booking-at="<?php echo addslashes($top_most_parent_post->post_title); ?>" data-booking-for="hotel">Book</a>
+											<?php
+											if(is_array($post_meta['crb_show_booking_button']) && !empty($post_meta['crb_show_booking_button'][0]) && $post_meta['crb_show_booking_button'][0] == 'yes')
+											{
+												?>
+												<a class="cstm-btn arrow-btn text-center popup-booking-button-anchor" href="javascript:void(0);" data-booking-at="<?php echo addslashes($top_most_parent_post->post_title); ?>" data-booking-for="hotel">Book</a>
+												<?php
+											}?>
 										</div>
 									</div>
 									<div class="col-9">
