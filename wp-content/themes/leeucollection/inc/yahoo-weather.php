b7c84365e84d557750c3b7c0a5e84ad19aa13e7b
Tanmay Vats <tanmay27vats@gmail.com>
2017-09-18 13:06:42 +0530

    weather icons

--- wp-content/themes/leeucollection/inc/yahoo-weather.php
+++ wp-content/themes/leeucollection/inc/yahoo-weather.php
@@ -67,12 +67,72 @@ class Display_Weather extends Yahoo_Weather
 	{
 		if($this->current != false)
 		{
-			echo '<div class="wthr-wrapper text-center">';
-	        /*echo '<div class="chat-icon-wrap"><img src="'.get_template_directory_uri().'/images/weather-icons/'.$this->current->code.'.png" /></div>';*/
-	        echo '<div class="chat-icon-wrap wi wi-icon-'.$this->current->code.'"></div>';
-	        echo '<span class="text-cht-wthr">'.$this->getTemp($this->current->temp).'</span>';
-	        echo '</div>';
+			$code = $this->current->code;
+			$hideWeatherForCodes = array(25,36,3200);
+
+			if(!in_array($code, $hideWeatherForCodes))
+			{
+				$icon = $this->getWeatherIcon($code);
+				echo '<div class="wthr-wrapper text-center">';
+		        echo '<div class="chat-icon-wrap"><img src="'.get_template_directory_uri().'/images/weather-icons/'.$icon.'" /></div>';
+		        /*echo '<div class="chat-icon-wrap wi wi-icon-'.$this->current->code.'"></div>';*/
+		        echo '<span class="text-cht-wthr">'.$this->getTemp($this->current->temp).'</span>';
+		        echo '</div>';
+		    }
 		}
+	}
+	private function getWeatherIcon($code)
+	{
+		$icon = "";
+		switch ($code)
+		{
+			case ($code >= 0 && $code <= 4):
+			case ($code >= 37 && $code <= 39):
+			case 45:
+					$icon = "storms.svg";
+					break;
+			case ($code >= 5 && $code <= 9):
+					$icon = "drizzle.svg";
+					break;
+			case ($code >= 10 && $code <= 12):
+			case 40:
+					$icon = "rain.svg";
+					break;
+			case ($code >= 13 && $code <= 16):
+			case ($code >= 41 && $code <= 43):
+			case ($code >= 46 && $code <= 47):
+					$icon = "snow.svg";
+					break;
+			case ($code >= 17 && $code <= 18):
+			case 35:
+					$icon = "hail.svg";
+					break;
+			case ($code >= 19 && $code <= 22):
+					$icon = "foggy.svg";
+					break;
+			case ($code >= 23 && $code <= 24):
+					$icon = "windy.svg";
+					break;
+			case 26:
+			case 28:
+			case 30:
+			case 44:
+					$icon = "cloudy-day.svg";
+					break;
+			case 27:
+			case 29:
+					$icon = "cloudy-night.svg";
+					break;
+			case 31:
+			case 33:
+					$icon = "clear.svg";
+					break;
+			case 32:
+			case 34:
+					$icon = "sunny.svg";
+					break;
+		}
+		return $icon;
 	}
 	private function getTemp($temperature)
 	{
