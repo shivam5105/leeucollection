e02f4bcd64a72f73f05f854c70a7339d74c296b8
Tanmay Vats <tanmay27vats@gmail.com>
2017-09-25 11:27:56 +0530

    changes

--- wp-content/themes/leeucollection/template-explore.php
+++ wp-content/themes/leeucollection/template-explore.php
@@ -146,6 +146,7 @@ get_header(); ?>
 											{
 												?>
 												filter: function(image) {
+													console.debug(image.tags);
 											    	if(image.tags.indexOf("<?php echo $hash_tag; ?>") >= 0)
 											    	{
 											    		image_count++;
