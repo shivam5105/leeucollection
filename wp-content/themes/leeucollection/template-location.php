ddaa34580e34ef813dce2c4a69f9b2f7239f7f48
Tanmay Vats <tanmay27vats@gmail.com>
2017-10-26 18:53:52 +0530

    changes

--- wp-content/themes/leeucollection/template-location.php
+++ wp-content/themes/leeucollection/template-location.php
@@ -74,8 +74,8 @@ get_header(); ?>
 					<?php
 				}?>
 				<div class="banner-text text-center pos-abs-top">
-					<div class="location-text ucase" itemprop="address"><?php echo $hotel_location; ?></div>
 					<h1 itemprop="name" class="ucase-h1"><?php echo $post->post_title;?></h1>
+					<div class="location-text ucase" itemprop="address"><?php echo $hotel_location; ?></div>
 				</div>
 			</div>
 			<div class="intro-blurb scroll-anim" data-anim="fade-up">
